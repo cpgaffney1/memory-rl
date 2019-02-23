56e06406a528cb8f37c853152f5656b5cf2c8640
Colin Gaffney <cgaffney@stanford.edu>
2019-02-22 17:20:26 -0800

    added harness to run maze memory network

--- configs/nature_memory.py
+++ configs/nature_memory.py
@@ -1,41 +1,44 @@
 class config():
     # env config
-    render_train     = False
-    render_test      = False
+    render_train = False
+    render_test = False
+    env_name = "Maze"
     overwrite_render = True
-    record           = False
-    high             = 255.
+    record = True
+    high = 255.
+    maze_size = 10
 
     # output config
-    output_path  = "results/nature/"
+    output_path = "results/train_nature_memory/"
     model_output = output_path + "model.weights/"
-    log_path     = output_path + "log.txt"
-    plot_output  = output_path + "scores.png"
+    log_path = output_path + "log.txt"
+    plot_output = output_path + "scores.png"
+    record_path = output_path + "monitor/"
 
     # model and training config
-    num_episodes_test = 20
-    grad_clip         = True
-    clip_val          = 10
-    saving_freq       = 5000
-    log_freq          = 50
-    eval_freq         = 100
-    soft_epsilon      = 0
+    num_episodes_test = 50
+    grad_clip = True
+    clip_val = 10
+    saving_freq = 250000
+    log_freq = 50
+    eval_freq = 250000
+    record_freq = 250000
+    soft_epsilon = 0.05
 
-    # hyper params
-    nsteps_train       = 1000
-    batch_size         = 32
-    buffer_size        = 500
-    target_update_freq = 500
-    gamma              = 0.99
-    learning_freq      = 4
-    state_history      = 4
-    lr_begin           = 0.00025
-    lr_end             = 0.0001
-    lr_nsteps          = nsteps_train/2
-    eps_begin          = 1
-    eps_end            = 0.01
-    eps_nsteps         = nsteps_train/2
-    learning_start     = 200
-    memory_unit_size   = 256
-    use_memory         = True
-    
+    # nature paper hyper params
+    nsteps_train = 5000000
+    batch_size = 32
+    buffer_size = 1000000
+    target_update_freq = 10000
+    gamma = 0.99
+    learning_freq = 4
+    state_history = 4
+    skip_frame = 4
+    lr_begin = 0.00025
+    lr_end = 0.00005
+    lr_nsteps = nsteps_train / 2
+    eps_begin = 1
+    eps_end = 0.1
+    eps_nsteps = 1000000
+    learning_start = 50000
+    use_memory = True
---
+++ train_maze_memory.py
@@ -0,0 +1,26 @@
+import gym
+from utils.preprocess import greyscale
+from utils.wrappers import PreproWrapper, MaxAndSkipEnv
+
+from schedule import LinearExploration, LinearSchedule
+from nature import NatureQN
+from utils.maze_env import EnvMaze
+
+from configs.nature_memory import config
+
+
+if __name__ == '__main__':
+    # make env
+    env = EnvMaze(n=config.maze_size)
+
+    # exploration strategy
+    exp_schedule = LinearExploration(env, config.eps_begin,
+            config.eps_end, config.eps_nsteps)
+
+    # learning rate schedule
+    lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
+            config.lr_nsteps)
+
+    # train model
+    model = NatureQN(env, config)
+    model.run(exp_schedule, lr_schedule)
