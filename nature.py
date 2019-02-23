b1a5cf2daaee9f54fabb5118e43b5ce3bf870893
Antonio <tantonio@stanford.edu>
2019-02-22 16:01:50 -0800

    implemented get_q_vals_with_memory, still need to incorporate with rest of code

--- nature.py
+++ nature.py
@@ -71,6 +71,68 @@ class NatureQN(Linear):
         return out
 
 
+    def get_q_values_and_memory_op(self, state, prev_memory, scope, reuse=False):
+        """
+        Returns Q values for all actions
+
+        Args:
+            state: (tf tensor) 
+                shape = (batch_size, img height, img width, nchannels)
+            scope: (string) scope name, that specifies if target network or not
+            reuse: (bool) reuse of variables in the scope
+            prev_memory: (tf tensor)
+            	shape = (batch_size, memory_unit_size)
+
+        Returns:
+            out: (tf tensor) of shape = (batch_size, num_actions)
+        """
+        # this information might be useful
+        num_actions = self.env.action_space.n
+
+        ##############################################################
+        """
+        TODO: implement the computation of Q values like in the paper
+                https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
+                https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf
+
+              you may find the section "model architecture" of the appendix of the 
+              nature paper particulary useful.
+
+              store your result in out of shape = (batch_size, num_actions)
+
+        HINT: 
+            - You may find the following functions useful:
+                - tf.layers.conv2d
+                - tf.layers.flatten
+                - tf.layers.dense
+
+            - Make sure to also specify the scope and reuse
+
+        """
+        ##############################################################
+        ################ YOUR CODE HERE - 10-15 lines ################ 
+
+        with tf.variable_scope(scope, reuse=reuse):
+            conv1 = tf.layers.conv2d(state, 32, 8, (4,4), 'same', activation=tf.nn.relu)
+            conv2 = tf.layers.conv2d(conv1, 64, 4, (2,2), 'same', activation=tf.nn.relu)
+            conv3 = tf.layers.conv2d(conv2, 64, 3, (1,1), 'same', activation=tf.nn.relu)
+            conv3 = tf.layers.flatten(conv3)
+
+            concat_memory = tf.concat([conv3,prev_memory],1)
+
+            h = tf.layers.dense(conv3, 512, activation=tf.nn.relu)
+
+            q_vals = tf.layers.dense(h, num_actions)
+            next_memory = tf.layers.dense(h, self.config.memory_unit_size)
+
+        vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
+        print(vars)
+        print()
+        ##############################################################
+        ######################## END YOUR CODE #######################
+        return q_vals, next_memory
+
+
 """
 Use deep Q network for test environment.
 """
