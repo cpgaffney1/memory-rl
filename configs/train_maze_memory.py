0423723b2066b05866692a04fbdb322c93c9976c
Colin Gaffney <cgaffney@stanford.edu>
2019-02-24 19:13:27 -0800

    some config changes

--- configs/train_maze_memory.py
+++ configs/train_maze_memory.py
@@ -28,14 +28,13 @@ class config():
 
     # nature paper hyper params
     nsteps_train = 5000000
-    batch_size = 32
+    batch_size = 64
     buffer_size = 1000000
     target_update_freq = 10000
-    memory_update_freq = 50000
+    memory_update_freq = 10000
     gamma = 0.99
     learning_freq = 4
     state_history = 4
-    skip_frame = 4
     lr_begin = 0.00025
     lr_end = 0.00005
     lr_nsteps = nsteps_train / 2
@@ -48,4 +47,4 @@ class config():
     # architecture config
     cnn_filters = [8]
     cnn_kernel = [3]
-    hidden_size = 16
+    hidden_size = 32
